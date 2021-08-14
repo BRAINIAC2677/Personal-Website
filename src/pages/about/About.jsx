ecf7ec7df6e89c96e37f615b158f95474c18a31b
Asif Azad <66885542+BRAINIAC2677@users.noreply.github.com>
2021-08-15 00:46:36 +0600

    feat(about, home): magic card, bg-circle

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -4,16 +4,29 @@ function About() {
   return (
     <div id="about" className="h-auto w-full p-12 bg-primary-light">
       <PageTitle title="About"></PageTitle>
-      <div className="flex">
-        <div className="hidden sm:block sm:w-1/2"></div>
-        <p className="sm:w-1/2 text-ink">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-          recusandae asperiores deserunt nam qui autem consequuntur similique
-          consequatur est? Numquam quos officiis illum quia libero sint eius
-          maiores voluptatum earum! Lorem, ipsum dolor sit amet consectetur
-          adipisicing elit. Eveniet recusandae asperiores deserunt nam qui autem
-          consequuntur similique consequatur est? Numquam quos officiis illum
-          quia libero sint eius maiores voluptatum earum!
+      <div className="flex justify-center items-center space-x-12 mt-24">
+        <div className="hidden lg:flex justify-center items-center magic-card mr-10 text-ink">
+          <p className="font-semibold text-2xl tracking-wider">
+            Life is beautiful
+          </p>
+        </div>
+        <div className="hidden lg:block h-32 w-1 bg-secondary"></div>
+        <p className="lg:w-1/2 text-ink">
+          This is Asif Azad. I was born in Faridpur, Bangladesh. Currently I am
+          living in Dhaka, Bangladesh. I am a Freshmen doing my undergad in
+          Bangladesh University of Engineering and Technology. My major is CSE.
+          My hobby is to watch and rewatch movies. And I love to play basketball
+          and table tennis.
+          <br />
+          <br /> I am enthusiastic about technology. I enjoy getting myself
+          acquinted with cutting-edge technologies. The wonders of technologies
+          fascinate me in every possible way. <br />
+          <br />I am optimistic about life. Life is a precious gift from the
+          Almighty. That is why I like to feel and enjoy every bit of it.
+          Moreover, I try to give the same vibe to others around me. <br />
+          <br />I love to meet, work and connect with new people. If they share
+          the same interests as me then it's good. And if they don't I get to
+          broaden my perspective engaging with them.
         </p>
       </div>
     </div>
--- src/pages/home/MainContent.jsx
+++ src/pages/home/MainContent.jsx
@@ -1,12 +1,17 @@
 function MainContent() {
   return (
-    <div className="center flex flex-col sm:flex-row-reverse justify-center sm:justify-around items-center sm:w-3/4">
-      <img
-        className="h-72 w-72 lg:h-96 lg:w-96  object-cover"
-        src="./images/hero.png"
-        alt="image"
-      />
-      <p className="pt-4 sm:pt-0 sm:pr-8 text-lg text-center sm:text-left text-ink uppercase tracking-wider">
+    <div className="center flex flex-col sm:flex-row-reverse justify-center sm:justify-around items-center mt-12 lg:mt-20 sm:w-3/4">
+      <div className="relative">
+        <div className="absolute top-1/3 left-1/2 h-80 w-80 lg:h-96 lg:w-96 rounded-full bg-secondary transform -translate-x-1/2 -translate-y-1/2">
+          <div className="absolute center h-60 w-60 lg:h-72 lg:w-72 rounded-full bg-primary-dark"></div>
+        </div>
+        <img
+          className="relative h-72 w-72 lg:h-96 lg:w-96  object-cover z-10"
+          src="./images/hero.png"
+          alt="image"
+        />
+      </div>
+      <p className="pt-4 sm:pt-0 sm:pr-8 text-lg text-center sm:text-left text-ink uppercase tracking-wider z-10">
         hello, i'm
         <span className="block py-2 text-4xl sm:text-5xl tracking-widest">
           asif azad
